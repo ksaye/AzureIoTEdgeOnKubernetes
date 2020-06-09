99543a9f7302049b6cc22fbbb07a680ab15dd0dd
ksaye <ksaye@saye.org>
2020-06-09 13:32:53 -0500

    Update workerNode.sh

--- workerNode.sh
+++ workerNode.sh
@@ -5,19 +5,13 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo "Installing the Prerequisites"
 sudo apt update 
-sudo apt install -y unzip tree apt-transport-https jq curl wget </dev/null
+sudo apt install -y unzip tree apt-transport-https jq curl wget docker.io </dev/null
+systemctl enable docker.service
 
-echo "Installing Microsoft's Moby Runtime"
-curl https://packages.microsoft.com/config/ubuntu/18.04/multiarch/prod.list > ./microsoft-prod.list
-sudo cp ./microsoft-prod.list /etc/apt/sources.list.d/
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo apt-get update 
-sudo apt-get install -y moby-engine moby-cli </dev/null
+swapoff -a
+sed -i -e '/swap.img/d' /etc/fstab
 
 echo "Installing Kubernetes Only"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 apt-add-repository "deb https://apt.kubernetes.io/ kubernetes-xenial main"
-swapoff -a
-sed -i -e '/swap.img/d' /etc/fstab
 apt-get install kubeadm -y </dev/null
