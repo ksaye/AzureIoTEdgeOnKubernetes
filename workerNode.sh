c7c2672de54af4e1e8b31081dc32c4fdf78fff1c
ksaye <ksaye@saye.org>
2020-06-09 17:07:30 -0500

    Update workerNode.sh

--- workerNode.sh
+++ workerNode.sh
@@ -3,15 +3,18 @@
 echo "Setting up a Kubernetes Worker Node"
 export DEBIAN_FRONTEND=noninteractive
 
+echo "adding the repositories"
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+apt-add-repository "deb https://apt.kubernetes.io/ kubernetes-xenial main"
+curl https://packages.microsoft.com/config/ubuntu/18.04/multiarch/prod.list > ./microsoft-prod.list
+sudo cp ./microsoft-prod.list /etc/apt/sources.list.d/
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
+
 echo "Installing the Prerequisites"
 sudo apt update 
-sudo apt install -y unzip tree apt-transport-https jq curl wget docker.io </dev/null
+sudo apt install -y unzip tree apt-transport-https jq moby-engine moby-cli kubeadm </dev/null
 systemctl enable docker.service
 
 swapoff -a
 sed -i -e '/swap.img/d' /etc/fstab
-
-echo "Installing Kubernetes Only"
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
-apt-add-repository "deb https://apt.kubernetes.io/ kubernetes-xenial main"
-apt-get install kubeadm -y </dev/null
