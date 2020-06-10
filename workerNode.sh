d60bd83d7cb987dd247a6d9969e30c27370ff6d1
ksaye <ksaye@saye.org>
2020-06-10 15:34:38 -0500

    Update workerNode.sh

--- workerNode.sh
+++ workerNode.sh
@@ -4,6 +4,7 @@ echo "Setting up a Kubernetes Worker Node"
 export DEBIAN_FRONTEND=noninteractive
 
 echo "adding the repositories"
+sudo apt update && sudo apt install curl apt-transport-https -y
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 apt-add-repository "deb https://apt.kubernetes.io/ kubernetes-xenial main"
 curl https://packages.microsoft.com/config/ubuntu/18.04/multiarch/prod.list > ./microsoft-prod.list
@@ -11,11 +12,9 @@ sudo cp ./microsoft-prod.list /etc/apt/sources.list.d/
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
 
-sleep 5
-
 echo "Installing the Prerequisites"
 sudo apt update 
-sudo apt install -y unzip tree apt-transport-https jq moby-engine moby-cli kubeadm </dev/null
+sudo apt install -y moby-engine moby-cli kubeadm </dev/null
 
 swapoff -a
 sed -i -e '/swap.img/d' /etc/fstab
